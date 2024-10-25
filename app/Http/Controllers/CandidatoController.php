085d9c7c43fb5c53ba750b506422d7f574df1011
Joao Victor <joaovictoruva18@gmail.com>
2024-10-25 17:52:58 -0300

    commit

--- app/Http/Controllers/CandidatoController.php
+++ app/Http/Controllers/CandidatoController.php
@@ -4,17 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Candidato;
+use App\Models\Cidade;
+use App\Models\Raca;
 class CandidatoController extends Controller
 {
     public function index()
-    {
+    {   
+        echo "Cidades: <br>";
+        $cidades = Cidade::all();
+        $informacoesCidades = $cidades->map(function ($cidade) {
+            return "ID: " . $cidade->cidade_id . ", Nome: " . $cidade->cidade ;
+        })->implode('<br>');
+        echo $informacoesCidades;
 
+        echo "<br><br>";
+
+        echo "Racas: <br>";
+        $racas = Raca::all();
+        $informacoesRacas = $racas->map(function ($raca) {
+            return "ID: " . $raca->raca_id . ", Nome: " . $raca->raca ;
+        })->implode('<br>');
+        echo $informacoesRacas;
+
+        echo "<br>";
+
+        echo "<br>Candidatos:<br>";
         $usuarios = Candidato::with(['cidade', 'raca'])->get();
 
         $informacoesUsuarios = $usuarios->map(function ($usuario) {
             return "ID: " . $usuario->id . ", Nome: " . $usuario->nome . ", Cidade: " . $usuario->cidade->cidade . ", Raça: " . $usuario->raca->raca;
-        })->implode('<br>'); // Junta todas as informações em uma única string com quebras de linha
-    
+        })->implode('<br>'); 
         echo $informacoesUsuarios;
 
     }
